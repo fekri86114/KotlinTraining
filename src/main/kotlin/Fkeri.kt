ba202a1d817f24a8e990a9a867312d3e89c77048
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-04 22:02:24 +0430

    You can train KOTLIN programming language with these files

---
+++ src/main/kotlin/Fkeri.kt
@@ -0,0 +1,72 @@
+//Loop - break - continue -  while - do while - array
+fun main() {
+    // Loop: -->
+//   for (i in 1..5) {
+//       for (j in 1..5) {
+//           println("*")
+//       }
+//       print("\n")
+//   }
+
+    // break: -->
+//    for (i in 1..500) {
+//
+//        if (i == 300) {
+//            break
+//        }
+//        println(i)
+//    }
+//    println("'for' finished!")
+
+    // continue: -->
+//    for (i in 1..20) {
+//
+//        if (i == 10) {
+//            continue
+//        }
+//        println(i)
+//    }
+
+    // while: -->
+//    var test = 10
+//    while ( test != 100 ) {
+//
+//        println(test)
+//        test++
+//    }
+//    println("'while' finished")
+
+    //do while
+//    var test = 10
+//
+//    do {
+//        println(test)
+//        test++
+//    } while (test != 100)
+
+    //array-0
+//    val scores = arrayOf(17, 20, 16, 14, 5, 9, 10, 11, 14, 13) // fixed
+//    scores.sort()
+//
+//    for (i in scores) {
+//
+//        println(i)
+//
+//    }
+
+    //arrayList
+//    val arrayList = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) // not fixed
+//    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) // immutable: yani nemitooni megdar va inaro taghir bedi
+
+//    val mutableList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// ************************************************************************
+//                                                                        *
+//     array        --=> fixed size                                       *
+//     arrayList    --=> not fixed                                        *
+//     List         --=> نوع ذخیره سازی متفاوتی داره                      *
+//     mutableList  --=> a list that can be change - mutable              *
+//                                                                        *
+// ************************************************************************
+
+
+}
