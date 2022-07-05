7014f22750e6b6b684cedfc0caa41c7ddf9d0386
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-05 10:12:36 +0430

    init commit

--- src/main/kotlin/MainActivity.kt
+++ src/main/kotlin/MainActivity.kt
@@ -1,3 +1,17 @@
 fun main() {
 
+    val arrayList = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) // not fixed
+//    val testList = arrayListOf<Int>(11, 12, 13, 14, 15, 16, 17, 18, 19, 20) // we created this list for arrayList.addAll( 'you can add testList here' )
+//    arrayList.add(11)
+//    arrayList.add(12)
+//    arrayList.addAll( testList ) // it (addAll) helps to add other lists to your list
+
+//    arrayList.clear() // you can clear the list with this method
+    arrayList.remove(5) // you can clear an item with this method
+
+
+    arrayList.forEach{
+        println(it)
+    }
+
 }
